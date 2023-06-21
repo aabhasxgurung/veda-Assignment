94e89b8bbb599ad232882ecbf520e78ee046f6b8
Axbhxs <aabhasgrg46@gmail.com>
2023-06-21 16:42:38 +0545

    few animations and design fixes

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,35 +1,45 @@
+"use client"
 import React from 'react';
-import './footer.css'
+import './footer.css';
+import { BsFacebook, BsInstagram } from 'react-icons/bs';
+import { AiFillTwitterCircle } from 'react-icons/ai';
+
+const scrollToTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
+};
 
 const Footer = () => {
     return (
         <div
-            className="flex justify-between text-white p-10 mt-60"
+            className="flex justify-between text-white p-10 mt-60 relative"
             style={{
                 backgroundImage: `url('/images/Rectangle 82.png')`,
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
                 backgroundRepeat: 'no-repeat',
-                height: '600px'
+                height: '600px',
             }}
         >
-            {/* <div className='grid'>
-
-            </div>
-            <div className='wrapper'>
-                <div className='circle'>
-
+            <div className="flex items-center">
+                <div className="flex flex-col">
+                    <img
+                        src="images/LOGO.png"
+                        alt="Logo"
+                        className="w-[139px] h-[64px] leading-[63.98px] ml-[35.16px]"
+                    />
+                    <p className="font-regular text-lg ml-4 w-[412.8px] h-[135px] mt-[35.16px]">
+                        LOGO is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                        standard dummy text ever since the 1500s, when
+                    </p>
                 </div>
-            </div> */}
-
-
-            <div className="flex items-center">
-                <h1 className="text-4xl font-bold font-northden text-[#F903AA]">LOGO</h1>
-                <p className="font-regular text-lg ml-4">This is my dummy page</p>
             </div>
-            <div className="flex items-center">
-                <div>
-                    <h1 className="text-lg text-[#F903AA] font-bold mb-2">Useful Links</h1>
+
+            <div className="flex items-center justify-center">
+                <div id="links" className="text-center absolute right-[44.83%] tracking-widest uppercase">
+                    <h1 className="font-bold mb-2 gradient-text leading-[30px] text-[25px] font-routhem">Useful Links</h1>
                     <a href="/" className="block mb-1">
                         Home
                     </a>
@@ -44,33 +54,30 @@ const Footer = () => {
                     </a>
                 </div>
             </div>
+
             <div className="flex items-center">
-                <div>
+                <div className="absolute right-[11.25%]">
                     <h1 className="text-lg font-bold mb-2">Follow Us on</h1>
-                    <div className="flex space-x-2">
-                        <a
-                            href="https://www.facebook.com/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <i className="fab fa-facebook text-xl"></i>
+                    <div className="flex space-x-2 items-center justify-center">
+                        <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+                            <BsFacebook size={40} />
                         </a>
-                        <a
-                            href="https://www.twitter.com/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <i className="fab fa-twitter text-xl"></i>
+                        <a href="https://www.twitter.com/" target="_blank" rel="noopener noreferrer">
+                            <AiFillTwitterCircle size={40} />
                         </a>
-                        <a
-                            href="https://www.instagram.com/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <i className="fab fa-instagram text-xl"></i>
+                        <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+                            <BsInstagram size={40} />
                         </a>
                     </div>
                 </div>
+            </div>
+
+            <a href="#" onClick={scrollToTop} className="cursor-pointer">
+                <img src="/images/Group 202.png" alt="Scroll to top" className="w-[108px] h-[184px] right-10" />
+            </a>
+
+            <div className="text-center absolute mt-8 bottom-0 left-0 right-0">
+                <p className="p-[39px] font-semibold">COPYRIGHT 2021 lOGO ALL RIGHT RESERVED</p>
             </div>
         </div>
     );
--- app/modules/product/ProductAll.js
+++ app/modules/product/ProductAll.js
@@ -1,78 +1,46 @@
 import ProductCard from '@/app/components/ProductCard';
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const ProductAll = () => {
-    const products = [
-        {
-            id: 1,
-            name: 'Product 1',
-            price: '$99',
-            imageSrc: 'images/p2 1.png',
-        },
-        {
-            id: 2,
-            name: 'Product 2',
-            price: '$99',
-            imageSrc: 'images/p2 1.png',
-        },
-        {
-            id: 3,
-            name: 'Product 3',
-            price: '$99',
-            imageSrc: 'images/p2 1.png',
-        },
-        {
-            id: 4,
-            name: 'Product 4',
-            price: '$99',
-            imageSrc: 'images/p2 1.png',
-        },
-        {
-            id: 5,
-            name: 'Product 5',
-            price: '$99',
-            imageSrc: 'images/p2 1.png',
-        },
-        {
-            id: 6,
-            name: 'Product 6',
-            price: '$99',
-            imageSrc: 'images/p2 1.png',
-        },
-        {
-            id: 7,
-            name: 'Product 7',
-            price: '$99',
-            imageSrc: 'images/p2 1.png',
-        },
-        {
-            id: 8,
-            name: 'Product 8',
-            price: '$99',
-            imageSrc: 'images/p2 1.png',
-        },
-        {
-            id: 9,
-            name: 'Product 9',
-            price: '$99',
-            imageSrc: 'images/p2 1.png',
-        },
+    const [products, setProducts] = useState([]);
 
-    ];
+    useEffect(() => {
+        // Fetch the product data when the component mounts
+        fetchProducts();
+    }, []);
+
+    const fetchProducts = async () => {
+        try {
+            // Make an HTTP GET request to the API endpoint
+            const response = await axios.get('https://dummyjson.com/products');
+            console.log(response.data.products)
+            // Set the fetched product data in the state
+            setProducts(response.data.products);
+        } catch (error) {
+            // Handle error if request fails
+            console.error('Error fetching products:', error);
+        }
+    };
+
     return (
         <div className="flex flex-col items-center mt-8">
             <div className="grid grid-cols-3 gap-20">
-                {products.map((product) => (
-                    <ProductCard
-                        key={product.id}
-                        name={product.name}
-                        imageSrc={product.imageSrc}
-                        price={product.price}
-                    />
-                ))}
+                {products.length > 0 ? (
+                    products.map((product) => (
+                        <ProductCard
+                            key={product.id}
+                            name={product.name}
+                            imageSrc={product.imageSrc}
+                            price={product.price}
+                        />
+                    ))
+                ) : (
+                    <p>Loading products...</p>
+                )}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default ProductAll
+export default ProductAll;
